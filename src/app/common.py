547a7ca308e78d279b1826716b035361d07c9846
jand6793 <jand6793@kettering.edu>
2023-05-10 10:32:29 -0400

    Added generic type to get_true_values

--- src/app/common.py
+++ src/app/common.py
@@ -1,5 +1,6 @@
-from typing import Any
+from typing import TypeVar
 
+T = TypeVar("T")
 
-def get_true_values(values: list[Any]):
+def get_true_values(values: list[T]) -> list[T]:
     return [v for v in values if v]
