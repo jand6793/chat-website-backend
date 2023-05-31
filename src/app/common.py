bbac599eedf4a45ff3d61469566ad25f6a9dcedc
jand6793 <jand6793@kettering.edu>
2023-05-30 21:04:58 -0400

    add flatten function

--- src/app/common.py
+++ src/app/common.py
@@ -1,4 +1,5 @@
 from typing import Any, Iterable, TypeVar
+import functools
 
 T = TypeVar("T")
 
@@ -17,3 +18,15 @@ def join_with_commas(values: Iterable[Any]):
 
 def unzip(items: Iterable[Iterable[T]]) -> list[list[T]]:
     return [list(t) for t in zip(*items)] if items else []
+
+
+def add_any(
+    container: tuple[Any, ...] | list[Any], value: Any
+) -> tuple[Any, ...] | list[Any]:
+    container_type = type(container)
+    return container + container_type(value)
+
+
+def flatten(container: tuple[Any, ...] | list[Any]):
+    container_type = type(container)
+    return container_type(functools.reduce(add_any, container, container_type()))
